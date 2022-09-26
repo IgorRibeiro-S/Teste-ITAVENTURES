f1ac28c65cbd328830f18a9dacc048f9fdb79bd1
Igor <igor_rsilva97@hotmail.com>
2022-09-26 00:55:23 -0300

    update PresencaController

--- src/main/java/com/teste/Controle/controllers/PresencaController.java
+++ src/main/java/com/teste/Controle/controllers/PresencaController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,8 +60,8 @@ public class PresencaController {
 
 	}
 
-	public ResponseEntity<Presenca> deletar(Long id) {
-		presencaService.buscarPorId(id);
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
 		presencaService.deletarPresenca(id);
 		return ResponseEntity.noContent().build();
 
